07485786b785c662bcc850ecef7146c04fe66463
Ben Pankow <ben@elementl.com>
2022-08-08 21:21:42 -0700

    Update __init__.py

--- my_dagster_project/assets/__init__.py
+++ my_dagster_project/assets/__init__.py
@@ -12,6 +12,6 @@ def cereals():
     return cereal_rows
 
 
-# NOTE: If you add another asset that depends on this one, to rematerilize it, you'll need to set up
-# an IO manager that can move the data across runs.
+# NOTE: To take advantage of Dagster's incremental re-execution functionality (e.g. retry from failure),
+# you'll need to set up an IO manager that can move the data across runs.
 # For more details, visit https://docs.dagster.io/concepts/io-management/io-managers#applying-io-managers-to-assets
